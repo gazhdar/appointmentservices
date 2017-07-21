69a969053e5debe40ef9147fcf0dfddcb33e5c1a
Rich Hildred <rhildred@ip-172-31-5-173.us-west-2.compute.internal>
2017-07-21 12:58:11 -0400

    new plugin

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Add Appointment Services
+*/
+function appointmentservices_shortcodes_init()
+{
+    function appointmentservice_shortcode($atts = [], $content = null)
+    {
+        $content .= file_get_contents(__DIR__ . "/appointmentServices.html");
+        return $content;
+    }
+    add_shortcode('appointmentservice-plugin', 'appointmentservice_shortcode');
+}
+add_action('init', 'appointmentservices_shortcodes_init');
